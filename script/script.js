3f4e991a5c8c186c435e5cbc3c03d8780e98514e
Carlos Eduardo Tussi <38245497+CarlosTussi@users.noreply.github.com>
2025-03-05 19:41:55 +0100

    init(personal-site) version 0.1

---
+++ script/script.js
@@ -0,0 +1,14 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("main-menu-js").style.width = "250px";
+    document.getElementById("bar-icon").style.display = "none";
+
+    
+}
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("main-menu-js").style.width = "0";
+    document.getElementById("bar-icon").style.display = "block";
+
+  }
