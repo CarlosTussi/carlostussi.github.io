0a863b873c9afd197a452c7d1ec457b48a20f6ef
Carlos Eduardo Tussi <38245497+CarlosTussi@users.noreply.github.com>
2025-03-06 11:33:22 +0100

    Fixing navigation bar for desktop

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("main-menu-js").style.width = "250px";
+    document.getElementById("mobile-menu-js").style.width = "250px";
     document.getElementById("bar-icon").style.display = "none";
 
     
@@ -8,7 +8,7 @@ function openNav() {
   
   /* Set the width of the side navigation to 0 */
   function closeNav() {
-    document.getElementById("main-menu-js").style.width = "0";
+    document.getElementById("mobile-menu-js").style.width = "0";
     document.getElementById("bar-icon").style.display = "block";
 
   }
